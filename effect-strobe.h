77215da774e443e661001bac8edcb48668665459
Adrien Thebo <git@somethingsinistral.net>
2015-02-19 08:06:01 -0800

    Add strobe effect

---
+++ effect-strobe.h
@@ -0,0 +1,26 @@
+#ifndef _STROBE_H_
+#define _STROBE_H_
+
+#include "effect.h"
+
+struct Strobe : Effect {
+
+    int counter = 0;
+
+    virtual void apply(Renderer *r) {
+        for(int i = 0; i < r->get_length(); i++) {
+            if (counter % 2 == 0) {
+                r->set_pixel(i, 0xFFFFFF);
+            } else {
+                r->clear_pixel(i);
+            }
+        }
+        counter++;
+    }
+
+protected:
+
+};
+
+#endif // _STROBE_H_
+
