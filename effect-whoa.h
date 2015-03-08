87dd646f135ce8885c6eb71818446aadba77d8d7
Adrien Thebo <git@somethingsinistral.net>
2015-03-07 21:28:55 -0800

    whoa: add luma scrolling

--- effect-whoa.h
+++ effect-whoa.h
@@ -6,15 +6,28 @@
 struct Whoa : Effect {
     
     float hue = 0;
-    float sat = 0.6;
-    float lum = 0.1;
+    //float sat = 0.9;
+    float sat = 0.0;
+    float lum = 0.3;
 
-    float scale = 0.003;
+    /*
+     * How much to increment the hue for each index. Higher values mean faster
+     * transitions through the spectrums, meaning a more compressed transition.
+     */
+    float hue_scale = 0.0101;
+
+    /*
+     * How much to decrement the luminosity for each index. Higher values mean
+     * smaller bands of light and dark. Very low values mean the entire panel
+     * will fade from light to dark.
+     */
+    float lum_scale = 0.0101;
 
     virtual void apply(Renderer *r) {
         Color c;
         for(int i = 0; i < r->get_length(); i++) {
             hue = increment_hue(hue);
+            lum = decrement_lum(lum);
             c.convert_hcl_to_rgb(hue, sat, lum);
             r->set_pixel(i, c.red, c.green, c.blue);
         }
@@ -23,9 +36,17 @@ struct Whoa : Effect {
 protected:
 
     float increment_hue(float current) {
-        float sum = current + scale;
+        float sum = current + hue_scale;
         if(sum >= 1.0) {
             sum--;
+        }
+        return sum;
+    }
+
+    float decrement_lum(float current) {
+        float sum = current - lum_scale;
+        if(sum <= 0.00001) {
+            sum += 0.4;
         }
         return sum;
     }
