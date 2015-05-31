483d5320d9344cfe114787640543010197f05a44
Adrien Thebo <git@somethingsinistral.net>
2015-05-31 15:16:13 -0700

    Add Pulse.h as single entry point

---
+++ Pulse.h
@@ -0,0 +1,23 @@
+/*
+ * Pulse.h
+ */
+
+#include <OctoWS2811.h>
+#include <Color.h>
+
+#include "grid.h"
+
+#include "output.h"
+#include "output-octo.h"
+#include "view.h"
+#include "framerate.h"
+
+#include "effect-random.h"
+#include "effect-warmrandom.h"
+#include "effect-coolrandom.h"
+#include "effect-emergency-lights.h"
+#include "effect-strobe.h"
+#include "effect-twinkle.h"
+#include "effect-whoa.h"
+#include "effect-rainbow.h"
+#include "effect-rgbwalk.h"
